34ce1d66f6eb1b81fd6dcb82174333f858035633
Vy Dinh Nguyen <vynguyen@Vys-MacBook-Pro.lan1>
2024-02-13 13:20:46 -0800

    build: added necessary tags for making the emoji

--- src/components/Emoji/index.tsx
+++ src/components/Emoji/index.tsx
@@ -1,6 +1,34 @@
 const Emoji = () => {
     const content = (
-        <></>
+        <main>
+            <article className="emoji">
+                <section className="head">
+                    <section className="eyes">
+                        <figure className="eye eye-left"></figure>
+                        <figure className="eye eye-right"></figure>
+                    </section>
+                    <figure className="mouth"></figure>
+                </section>
+            </article>
+            <article className="emoji">
+                <section className="head">
+                    <section className="eyes">
+                        <figure className="eye eye-left"></figure>
+                        <figure className="eye eye-right"></figure>
+                    </section>
+                    <figure className="mouth"></figure>
+                </section>
+            </article>
+            <article className="emoji">
+                <section className="head">
+                    <section className="eyes">
+                        <figure className="eye eye-left"></figure>
+                        <figure className="eye eye-right"></figure>
+                    </section>
+                    <figure className="mouth"></figure>
+                </section>
+            </article>
+        </main>
     );
 
     return content;
